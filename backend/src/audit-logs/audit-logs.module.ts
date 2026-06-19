221a017e09573b6c3626f2e6e1bfeb0183a45bbf
Gideon Sleek <kapyengadeux27@gmail.com>
2026-06-19 04:44:28 +0300

    Update audit-logs.module.ts

--- backend/src/audit-logs/audit-logs.module.ts
+++ backend/src/audit-logs/audit-logs.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuditLogsService } from './audit-logs.service';
 import { AuditLogsController } from './audit-logs.controller';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [AuditLogsService],
   controllers: [AuditLogsController],
   exports: [AuditLogsService],
