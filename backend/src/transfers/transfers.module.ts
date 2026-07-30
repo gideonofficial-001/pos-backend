06c1dc4e16b4ae54d9a8ef6581b943aecab9e8f8
Gideon Sleek <kapyengadeux27@gmail.com>
2026-07-30 11:18:31 +0300

    Add PrismaModule to TransfersModule imports

--- backend/src/transfers/transfers.module.ts
+++ backend/src/transfers/transfers.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TransfersService } from './transfers.service';
 import { TransfersController } from './transfers.controller';
 import { NotificationsModule } from '../notifications/notifications.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [NotificationsModule],
+  imports: [NotificationsModule, PrismaModule],
   providers: [TransfersService],
   controllers: [TransfersController],
   exports: [TransfersService],
