7bdbb70d2bd1f8af20704f2cf1fc59318d3f13f6
Gideon Sleek <kapyengadeux27@gmail.com>
2026-08-01 06:41:46 +0300

    Add TransferService and TransferController to InventoryModule

--- backend/src/inventory/inventory.module.ts
+++ backend/src/inventory/inventory.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { InventoryService } from './inventory.service';
 import { InventoryController } from './inventory.controller';
+import { TransferService } from './transfer.service';
+import { TransferController } from './transfer.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
-  providers: [InventoryService],
-  controllers: [InventoryController],
-  exports: [InventoryService],
+  providers: [InventoryService, TransferService],
+  controllers: [InventoryController, TransferController],
+  exports: [InventoryService, TransferService],
 })
 export class InventoryModule {}
