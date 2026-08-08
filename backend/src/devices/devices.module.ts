d609270add2f139dc5ea60fb4740ce0936368700
Gideon Sleek <kapyengadeux27@gmail.com>
2026-08-09 01:24:59 +0300

    Add NotificationsModule to DevicesModule imports

--- backend/src/devices/devices.module.ts
+++ backend/src/devices/devices.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { DevicesService } from './devices.service';
 import { DevicesController } from './devices.controller';
 import { AuditLogsModule } from '../audit-logs/audit-logs.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
-  imports: [AuditLogsModule],
+  imports: [AuditLogsModule, NotificationsModule],
   providers: [DevicesService],
   controllers: [DevicesController],
   exports: [DevicesService],
